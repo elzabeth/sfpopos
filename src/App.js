144a75dd677c7a29948b65b390e7b5354e40ad28
Elizabeth R <elzaabeth@gmail.com>
2021-11-10 20:44:17 +0530

    added-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from 'react'
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
+
 import Title from './Title'
+import About from './About'
 import POPOSList from './POPOSList'
 import Footer from './Footer'
 
 export default function App() {
-  return (
-    <div className="App">
-          <Title />
-          <POPOSList />
-          <Footer/>
-      </div>
+    return (
+        <div className="App">
+            <Title />
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={POPOSList} />
+                    <Route exact path="/about" component={About} />
+                </Switch>
+            </Router>
+            <Footer />
+        </div> 
   );
 }
